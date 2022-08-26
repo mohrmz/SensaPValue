671813e14c974070ed5323f089bdcb66b9bc4596
mohammad0ramezani <mohammad0ramezani@outlook.com>
2022-08-26 13:08:51 +0430

    feet(ttest_ind) : calculate ttest_ind

--- SensaPValue.py
+++ SensaPValue.py
@@ -3,7 +3,13 @@ import pandas as pd
 from scipy.stats import ttest_ind, ranksums 
 
 df = pd.read_excel('Sensa.xlsx')
-
-s, p = ttest_ind(df[criterion_].values, df[criterion_].values, alternative="greater")
+columns = df.columns.values
+matching = ['per','rec','f1']
+for match in matching:
+    print(match)
+    s1, p1 = ttest_ind(df['our('+match+')'].values, df['cod2vec('+match+')'].values, alternative="greater")
+    print('cod2vec :' +str(p1))
+    s2, p2 = ttest_ind(df['our('+match+')'].values, df['cod2seq('+match+')'].values, alternative="greater")
+    print('cod2seq :' +str(p2))
 
 print('end')
